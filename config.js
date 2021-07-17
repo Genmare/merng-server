9fa0d80f15fc9d2bf53a57f3628ebd8a6ca09693
Genmare <gregfi@aliceadsl.fr>
2021-07-17 23:29:13 +0200

    Revert "add config file"
    This reverts commit 1066b7f06b3e8e7b6477e6ed18c020e4ac4b31fe.

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGODB:
-        "mongodb+srv://Genmare:VtAlKCODC1F2ziY0@go-fullstack.amdtq.mongodb.net/merng?retryWrites=true&w=majority",
-    SECRET_KEY: "Ma clé secrète",
-};

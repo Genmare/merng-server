1066b7f06b3e8e7b6477e6ed18c020e4ac4b31fe
Genmare <gregfi@aliceadsl.fr>
2021-07-17 23:05:50 +0200

    add config file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+        "mongodb+srv://Genmare:VtAlKCODC1F2ziY0@go-fullstack.amdtq.mongodb.net/merng?retryWrites=true&w=majority",
+    SECRET_KEY: "Ma clé secrète",
+};
